620dd75beedaef531cf05e6c4ef081f202d1d292
Angelo Breuer <angelobr@outlook.de>
2020-04-19 17:45:28 +0200

    Fix Weather condition types (Fixes #2)

--- src/Entities/WeatherConditionType.cs
+++ src/Entities/WeatherConditionType.cs
@@ -3,51 +3,82 @@
     /// <summary>
     ///     The different weather condition types.
     /// </summary>
+    /// <remarks>https://openweathermap.org/weather-conditions</remarks>
     public enum WeatherConditionType : byte
     {
         /// <summary>
-        ///     Thunderstorm weather condition
+        ///     Denotes that the weather condition type is <c>"Ash"</c>.
         /// </summary>
-        Thunderstorm,
+        Ash,
 
         /// <summary>
-        ///     Drizzle weather condition
-        /// </summary>
-        Drizzle,
-
-        /// <summary>
-        ///     Rain weather condition
-        /// </summary>
-        Rain,
-
-        /// <summary>
-        ///     Snow weather condition
-        /// </summary>
-        Snow,
-
-        /// <summary>
-        ///     Atmosphere weather condition
-        /// </summary>
-        Atmosphere,
-
-        /// <summary>
-        ///     Clear weather condition
+        ///     Denotes that the weather condition type is <c>"Clear"</c>.
         /// </summary>
         Clear,
 
         /// <summary>
-        ///     Clouds weather condition
+        ///     Denotes that the weather condition type is <c>"Clouds"</c>.
         /// </summary>
         Clouds,
 
         /// <summary>
-        ///     Haze weather condition
+        ///     Denotes that the weather condition type is <c>"Drizzle"</c>.
+        /// </summary>
+        Drizzle,
+
+        /// <summary>
+        ///     Denotes that the weather condition type is <c>"Dust"</c>.
+        /// </summary>
+        Dust,
+
+        /// <summary>
+        ///     Denotes that the weather condition type is <c>"Fog"</c>.
+        /// </summary>
+        Fog,
+
+        /// <summary>
+        ///     Denotes that the weather condition type is <c>"Haze"</c>.
         /// </summary>
         Haze,
 
         /// <summary>
-        ///     Mist weather condition
+        ///     Denotes that the weather condition type is <c>"Mist"</c>.
         /// </summary>
-        Mist
+        Mist,
+
+        /// <summary>
+        ///     Denotes that the weather condition type is <c>"Rain"</c>.
+        /// </summary>
+        Rain,
+
+        /// <summary>
+        ///     Denotes that the weather condition type is <c>"Sand"</c>.
+        /// </summary>
+        Sand,
+
+        /// <summary>
+        ///     Denotes that the weather condition type is <c>"Smoke"</c>.
+        /// </summary>
+        Smoke,
+
+        /// <summary>
+        ///     Denotes that the weather condition type is <c>"Snow"</c>.
+        /// </summary>
+        Snow,
+
+        /// <summary>
+        ///     Denotes that the weather condition type is <c>"Squall"</c>.
+        /// </summary>
+        Squall,
+
+        /// <summary>
+        ///     Denotes that the weather condition type is <c>"Thunderstorm"</c>.
+        /// </summary>
+        Thunderstorm,
+
+        /// <summary>
+        ///     Denotes that the weather condition type is <c>"Tornado"</c>.
+        /// </summary>
+        Tornado
     }
 }
